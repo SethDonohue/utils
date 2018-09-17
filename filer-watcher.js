407ff751865a0a9a90ab6bf00c7a434881edb744
Seth Donohue <sdonohue@paciolan.com>
2018-09-17 13:40:34 -0700

    exec working on process.argv

--- filer-watcher.js
+++ filer-watcher.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
+const exec = require('child_process').exec;
 
-const filePath = 'C://Users/sdonohue/PhpstormProjects/leetcode/multiply-strings.js';
+const filePath = process.argv[2];
+const testFilePath = process.argv[3];
 const file = fs.readFileSync(filePath);
 
 console.log('File: ', file);
+console.log('Test File: ' , testFilePath);
 
 fs.watch(filePath, (event, filename) => {
 	if(filename){
-		console.log(event, ' ', filename, ':');
-		
+		console.log('========== ' + event + ' to ' + filename + ' ===========');
+		// Run the test file...
+		exec(`node ` + testFilePath, (error, stdout, stderr) => {
+			console.log('stdout: ', stdout);
+		});
 	}else{
 		console.error('File name not provided!');
 	}
