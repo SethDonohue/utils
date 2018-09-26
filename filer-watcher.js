67068dcfb0f0d1134bcf05cd7c1972d43fe24fc8
Seth Donohue <sdonohue@paciolan.com>
2018-09-26 11:11:08 -0700

    switched command line args to watch multiple files with one test file

--- filer-watcher.js
+++ filer-watcher.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
 const spawn = require('child_process').spawn;
 
-const filePath = process.argv[2];
-const testFilePath = process.argv[3];
-const file = fs.readFileSync(filePath);
+// Run this file (typically a test file) from the first command line argument
+// file path when any of the remaining command line argument file paths are changed.
+let fileToRun = process.argv[2]; 
+console.log('Test File: ' , fileToRun);
 
-console.log('Test File: ' , testFilePath);
+const watcher = (filePath) => {
+	const file = fs.readFileSync(filePath);
 
-fs.watch(filePath, (event, filename) => {
-	if(filename){
-		console.log('========== ' + event + ' to ' + filename + ' ===========');
-		spawn(`node`, [testFilePath], { stdio: 'inherit' });
-	}else{
-		console.error('File name not provided!');
-	}
-});
+	fs.watch(filePath, (event, filename) => {
+		if(filename){
+			console.log('========== ' + event + ' to ' + filename + ' ===========');
+			spawn(`node`, [fileToRun], { stdio: 'inherit' });
+		}else{
+			console.error('File name not provided!');
+		}
+	});
+}
+
+// If there are man files to watch loop over and create a new watcher for each one.
+for(let i = 2; i <= process.argv.length - 1; i++){
+	console.log('New watcher for: ', process.argv[i]);
+	watcher(process.argv[i]);
+}
